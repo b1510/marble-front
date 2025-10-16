29808d52d5c62627f076b86b21bec74b805c3bd7
Antho <bisiostyle@hotmail.com>
2025-10-16 18:50:31 +0200

    Add docker dockerisation

--- src/api/marbleApi.ts
+++ src/api/marbleApi.ts
@@ -11,7 +11,7 @@ export interface Marble {
     imageUrl?: string;
 }
 
-const API_URL = 'https://localhost:5001/api/marbles';
+const API_URL = 'https://api.samyhama.fr/api/marbles';
 
 export const marbleApi = {
     async getAll(): Promise<Marble[]> {
